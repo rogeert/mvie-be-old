3d7c34c1080a286d797929f5f89c106be8623aaa
rogeert <rogert_rekaj@hotmail.it>
2022-10-26 08:23:31 +0200

    lobbyservice update

--- src/main/kotlin/com/rogeert/mviebe/services/lobby/LobbyService.kt
+++ src/main/kotlin/com/rogeert/mviebe/services/lobby/LobbyService.kt
@@ -1,7 +1,14 @@
 package com.rogeert.mviebe.services.lobby
 
+import com.rogeert.mviebe.util.Response
+import com.rogeert.mviebe.websocket.dto.LobbyDto
+
 interface LobbyService {
 
+    fun createLobby():Response<LobbyDto>
 
+    fun joinLobby(code:String,username:String):Response<LobbyDto>
+
+    fun leaveLobby(code:String,username:String):Response<LobbyDto>
 
 }
--- src/main/kotlin/com/rogeert/mviebe/services/lobby/LobbyServiceImpl.kt
+++ src/main/kotlin/com/rogeert/mviebe/services/lobby/LobbyServiceImpl.kt
@@ -1,4 +1,18 @@
 package com.rogeert.mviebe.services.lobby
 
-class LobbyServiceImpl: LobbyService {
+import com.corundumstudio.socketio.SocketIOServer
+import com.rogeert.mviebe.models.Lobby
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+
+@Service
+class LobbyServiceImpl(private final val server: SocketIOServer): LobbyService {
+
+    private final val logger = LoggerFactory.getLogger(LobbyServiceImpl::class.java)
+    private final val lobbies = hashMapOf<String, Lobby>()
+    init {
+
+    }
+
 }
